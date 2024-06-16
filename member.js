84b2d20f5762c3def3abff8fdee3b520a1678ef1
Weihe Zheng <wayzheng@usc.edu>
2024-06-16 06:20:03 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,12 @@
-function skillMember(){
-    
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skillsMember/views/skills-member.client.view.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
 }
